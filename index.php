54c37d1b90101fb2e2c733d7ad46b300083808f9
yohannmts <yohannmats@gmail.com>
2020-11-09 10:52:30 +0100

    add gitignore

---
+++ index.php
@@ -0,0 +1,7 @@
+<!-- GIT est un outil de versionnig -->
+
+<!-- github est ce que l'on appelle un hébegeur de repositories git -->
+<!-- git init a faire une fois au début -->
+<!-- git status -->
+<!-- git add * ou git add --all -->
+ <!--git commit -m "le message"  -->
